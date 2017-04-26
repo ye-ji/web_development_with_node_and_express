724e2aea11f03d64ac421eb5ad656af39f0c9fae
yj.lee <yj.lee@ysmcorp.com>
2017-04-26 15:09:00 +0900

    add flash. (doesn't work!!)

--- server.js
+++ server.js
@@ -22,12 +22,79 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 
+// credential
+var credentials = require('./credentials');
+
+app.use(require('cookie-parser')(credentials.coockieSecret));
+// session
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialize: false,
+    secret: credentials.coockieSecret
+}));
+
+
+
 // middleware
 // test mode
 app.use(function(req, res, next) {
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
 	next();
 });
+
+
+// flash message middleware
+app.use(function(req, res, next) {
+    // 플래시 메세지가 있다면 콘텍스트에 전달한 다음 지웁니다.
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res) {
+    var name = req.body.name || '',
+        email = req.body.email || '';
+    // 입력 유효성 검사
+    if(!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr)
+            return res.json({error: 'Invalid name email adress.'});
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid!',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+
+    // NewsletterSignup은 내가 만들게 될 객체 예제!
+    // 프로젝트에따라 정확한 구현 내용이나 인터페이스는 모두 달라질테고, 그건 나의 몫!
+    // 일반적인 익스프레스 프로그램이어떤모양인지 참고만 하시게나!
+    new NewsletterSignup({name: name, email: email}).save(function (err)  {
+       if(err) {
+           if(req.xhr) return res.json({error: 'Database error'});
+           req.session.flash ={
+               type: 'danger',
+               intro: 'Database error',
+               message: 'There was a database error; please try again later',
+           }
+           return res.redirect(303, '/newsletter/archive');
+       }
+
+       if(req.xhr) return res.json({success: true});
+       req.session.flash = {
+           type: 'success',
+           intro: 'Thank you!',
+           message: 'You have now been signed up for the newsletter',
+       };
+       return res.redirect(303, '/newsletter/archive');
+    });
+})
+
+
+
+
 
 
 // mocked weather data
@@ -154,9 +221,6 @@ app.use('/upload', function(req, res, next) {
     })(req, res, next);
 });
 
-
-
-
 app.post('/process', function(req, res){
 
     /*console.log('Form (from querystring): ' + req.query.form);
@@ -170,8 +234,7 @@ app.post('/process', function(req, res){
     } else {
         res.redirect(303, '/thank-you');
     }
-})
-
+});
 
 
 
